2ea266e606d3632450412f6255bca03791e613ac
PhilRegnier <phil.regnier@gmail.com>
2022-04-12 14:23:59 +0200

    formulaire participant formulaire sortie... wip confirmation mot de passe modif profil

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
-use App\Form\ProfilFormType;
+use App\Form\ParticipantFormType;
 use App\Repository\ParticipantRepository;
 use App\Security\AppAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
@@ -14,17 +14,10 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
+#[Route('/participant', name: 'participant')]
 class ParticipantController extends AbstractController
 {
-    #[Route('/participant', name: 'app_participant')]
-    public function index(): Response
-    {
-        return $this->render('participant/index.html.twig', [
-            'controller_name' => 'ParticipantController',
-        ]);
-    }
-
-    #[Route('/participant/profil{id}', name: 'participant_profil', requirements: ["id" => "\d+"])]
+    #[Route('/profil{id}', name: '_profil', requirements: ["id" => "\d+"])]
     public function monProfil(
         Request $request,
         UserPasswordHasherInterface $userPasswordHasher,
@@ -40,29 +33,35 @@ class ParticipantController extends AbstractController
         $user->setTelephone($this->getUser()->getTelephone());
         $user->setMail($this->getUser()->getMail());
         $user->setCampus($this->getUser()->getCampus());
-        $form = $this->createForm(ProfilFormType::class, $user);
+
+        $form = $this->createForm(ParticipantFormType::class, $user);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setMotPasse(
-                $userPasswordHasher->hashPassword(
+            if ($form->get('plainPassword') === $form->get('confirmation')) {
+                $user->setMotPasse(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Votre profil a été modifié avec succès.');
+
+                return $userAuthenticator->authenticateUser(
                     $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-
-            $entityManager->flush();
-            // do anything else you need here, like send an email
-
-            return $userAuthenticator->authenticateUser(
-                $user,
-                $authenticator,
-                $request
-            );
+                    $authenticator,
+                    $request
+                );
+            }
+            else {
+                $this->addFlash('warning', 'Les mots de passe saisis sont différents.');
+            }
         }
         return $this->render('participant/profil.html.twig', [
-            'registrationForm' => $form->createView(),
+            'participantForm' => $form->createView(),
         ]);
     }
 }
--- src/Form/ParticipantFormType.php
+++ src/Form/ParticipantFormType.php
@@ -12,7 +12,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class ProfilFormType extends AbstractType
+class ParticipantFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
