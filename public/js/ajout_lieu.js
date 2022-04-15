66ee1d05fbd5a1b477095a0eb299d8b7aeb55ce7
PhilRegnier <phil.regnier@gmail.com>
2022-04-15 14:28:40 +0200

    création lieu avec lien depuis ajout sortie. ATTENTION: modif dans les scripts javascipt => il faut modifier BASE_URL !

---
+++ public/js/ajout_lieu.js
@@ -0,0 +1,24 @@
+
+$(document).ready(updateLieu());
+
+document.getElementById('ville').addEventListener('change', updateLieu);
+
+function updateLieu() {
+
+    let villeId = document.getElementById('ville').value;
+
+    $.ajax(
+        {
+            url: BASE_URL + '/villes/' + villeId,
+            method: 'GET'
+        }
+    )
+        .done(
+            (ville) => {
+                $('#codePostal').text(ville.codePostal);
+            }
+        )
+        .fail()
+        .always();
+}
+
